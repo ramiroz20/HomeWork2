c7bae467bc9257b96495779ddc5bb6d4b4c92015
Roman <romantuser69@gmail.com>
2022-02-04 14:45:25 +0500

    There are three words

---
+++ src/main/java/ru/homeworkApp/com/HomeWorkApp.java
@@ -0,0 +1,19 @@
+package ru.homeworkApp.com;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+
+        printThreeWords(Apple);
+    }
+
+
+        System.out.println(
+
+    static void printThreeWords());
+
+    }
+
+
+
+}
+
